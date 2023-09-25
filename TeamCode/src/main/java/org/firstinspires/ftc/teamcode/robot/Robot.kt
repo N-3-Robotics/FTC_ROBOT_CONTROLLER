de6e46a052c765f7e9de6ef619e683093c44a32d
Alejandro <aescamilla.2025@carmelhs.org>
2023-09-25 16:28:17 -0500

    Fixed some bits, robot drives

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Robot.kt
@@ -57,13 +57,57 @@ class Robot(hwMap: HardwareMap?) {
 
     private var headingCorrection: Double = 0.0
 
-    var lastAngle: Double = botHeading
+//    var lastAngle: Double = botHeading
 
-    var globalAngle: Double = botHeading
+//    var globalAngle: Double = botHeading
 
     private var hasBeenRun = true
 
     var autoMode = UNKNOWN
+
+    private var hardwareMap: HardwareMap? = null
+
+    var headingPIDController = PIDController(heading_kP, heading_kI, heading_kD)
+    var turnPIDController = PIDController(turn_kP, turn_kI, turn_kD)
+    var drivePIDController = PIDController(drive_kP, drive_kI, drive_kD)
+
+    init {
+        hardwareMap = hwMap
+
+
+        FL = hardwareMap!!.get(DcMotorEx::class.java, "FL")
+        FR = hardwareMap!!.get(DcMotorEx::class.java, "FR")
+        BL = hardwareMap!!.get(DcMotorEx::class.java, "BL")
+        BR = hardwareMap!!.get(DcMotorEx::class.java, "BR")
+
+        driveMotors = arrayOf(FL, FR, BL, BR)
+
+        headingPIDController.setOutputBounds(-0.1, 0.1)
+        turnPIDController.setOutputBounds(-1.0, 1.0)
+        drivePIDController.setOutputBounds(-1.0, 1.0)
+
+
+        FL.direction = DcMotorSimple.Direction.REVERSE
+        BL.direction = DcMotorSimple.Direction.REVERSE
+
+
+
+        FR.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        FL.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        BR.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+        BL.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
+
+        FR.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        FL.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        BR.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        BL.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+
+        IMU = hardwareMap!!.get(BNO055IMU::class.java, "imu")
+        val parameters = BNO055IMU.Parameters()
+        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
+        IMU.initialize(parameters)
+    }
 
     val currentPosition: Int
         get() {
@@ -80,24 +124,24 @@ class Robot(hwMap: HardwareMap?) {
             return false
         }
 
-    val botHeading: Double
-        get() {
-            val currentAngle: Double = radToDeg(IMU.angularOrientation.firstAngle)
-
-            var deltaAngle = currentAngle - lastAngle
-
-            if (deltaAngle < -180) {
-                deltaAngle += 360
-            } else if (deltaAngle > 180) {
-                deltaAngle -= 360
-            }
-
-            globalAngle += deltaAngle
-
-            lastAngle = currentAngle
-
-            return globalAngle
-        }
+//    val botHeading: Double
+//        get() {
+//            val currentAngle: Double = radToDeg(IMU.angularOrientation.firstAngle)
+//
+//            var deltaAngle = currentAngle - lastAngle
+//
+//            if (deltaAngle < -180) {
+//                deltaAngle += 360
+//            } else if (deltaAngle > 180) {
+//                deltaAngle -= 360
+//            }
+//
+//            globalAngle += deltaAngle
+//
+//            lastAngle = currentAngle
+//
+//            return globalAngle
+//        }
 
     var zeroPowerBehavior: DcMotor.ZeroPowerBehavior
         get() {
@@ -121,11 +165,7 @@ class Robot(hwMap: HardwareMap?) {
 
 
 
-    private var hardwareMap: HardwareMap? = null
 
-    var headingPIDController = PIDController(heading_kP, heading_kI, heading_kD)
-    var turnPIDController = PIDController(turn_kP, turn_kI, turn_kD)
-    var drivePIDController = PIDController(drive_kP, drive_kI, drive_kD)
 
     private fun tDrive(drive: Double, turn: Double){
         FL.power = drive + turn
@@ -134,7 +174,7 @@ class Robot(hwMap: HardwareMap?) {
         BR.power = drive - turn
 
         autoMode = MANUAL
-        update()
+//        update()
     }
 
     fun RCDrive(y: Double, x: Double, rx: Double) {
@@ -158,15 +198,15 @@ class Robot(hwMap: HardwareMap?) {
 //        update()
     }
 
-    fun FCDrive(y: Double, x: Double, turn: Double) {
-        /*val x = x * strafeMultiplier
-        val rotX = x * cos(-botHeading) - y * sin(-botHeading)
-        val rotY = y * sin(-botHeading) + x * cos(-botHeading)*/
-
-        val input = Vector2d(y, -x).rotated(-botHeading)
-
-        RCDrive(input.y, input.x, turn)
-    }
+//    fun FCDrive(y: Double, x: Double, turn: Double) {
+//        /*val x = x * strafeMultiplier
+//        val rotX = x * cos(-botHeading) - y * sin(-botHeading)
+//        val rotY = y * sin(-botHeading) + x * cos(-botHeading)*/
+//
+//        val input = Vector2d(y, -x).rotated(-botHeading)
+//
+//        RCDrive(input.y, input.x, turn)
+//    }
 
     fun gamepadDrive(controller: Gamepad, multiplier: Double) {
         RCDrive(
@@ -176,43 +216,43 @@ class Robot(hwMap: HardwareMap?) {
         )
     }
 
-    fun update() {
-        when (autoMode) {
-            UNKNOWN -> {
-                stop()
-            }
-            TURN -> {
-                while (abs(errorAngle) > AutoTurnTolerance) {
-                    errorAngle = angleTarget - botHeading
-
-                    correction = turnPIDController.update(errorAngle)
-
-                    tDrive(0.0, correction)
-                }
-                stop()
-            }
-            STRAIGHT -> {
-                while (abs(distanceError) > AutoDriveTolerance) {
-                    headingError = headingTarget - botHeading
-
-                    headingCorrection = headingPIDController.update(headingError)
-
-                    distanceError = distanceTarget - ticksToInches(currentPosition)
-
-                    correction = drivePIDController.update(distanceError)
-
-                    tDrive(correction, headingCorrection)
-                }
-                stop()
-            }
-            MANUAL -> {
-
-            }
-        }
-    }
+//    fun update() {
+//        when (autoMode) {
+//            UNKNOWN -> {
+//                stop()
+//            }
+//            TURN -> {
+//                while (abs(errorAngle) > AutoTurnTolerance) {
+//                    errorAngle = angleTarget - botHeading
+//
+//                    correction = turnPIDController.update(errorAngle)
+//
+//                    tDrive(0.0, correction)
+//                }
+//                stop()
+//            }
+//            STRAIGHT -> {
+//                while (abs(distanceError) > AutoDriveTolerance) {
+//                    headingError = headingTarget - botHeading
+//
+//                    headingCorrection = headingPIDController.update(headingError)
+//
+//                    distanceError = distanceTarget - ticksToInches(currentPosition)
+//
+//                    correction = drivePIDController.update(distanceError)
+//
+//                    tDrive(correction, headingCorrection)
+//                }
+//                stop()
+//            }
+//            MANUAL -> {
+//
+//            }
+//        }
+//    }
 
     private fun resetHeading(){
-        globalAngle = 0.0
+        var globalAngle = 0.0
     }
 
     fun stop() {
@@ -226,21 +266,6 @@ class Robot(hwMap: HardwareMap?) {
             motor.mode = DcMotor.RunMode.RUN_USING_ENCODER
         }
     }
-    fun straight(inches: Double){
-        hasBeenRun = false
-        prepareMotors()
-
-        distanceTarget = inches
-
-        distanceError = distanceTarget
-
-        autoMode = STRAIGHT
-
-        headingTarget = botHeading
-
-        update()
-    }
-
     fun turnRight(angle: Int = -90){
         turnPIDController.reset()
 
@@ -252,8 +277,23 @@ class Robot(hwMap: HardwareMap?) {
 
         prepareMotors()
 
-        update()
+//        update()
     }
+
+//    fun straight(inches: Double){
+//        hasBeenRun = false
+//        prepareMotors()
+//
+//        distanceTarget = inches
+//
+//        distanceError = distanceTarget
+//
+//        autoMode = STRAIGHT
+//
+//        headingTarget = botHeading
+//
+//        update()
+//    }
 
     fun turnLeft(angle: Int = 90){
         turnPIDController.reset()
@@ -266,7 +306,7 @@ class Robot(hwMap: HardwareMap?) {
 
         prepareMotors()
 
-        update()
+//        update()
     }
 
     fun rumble(controller: Gamepad, side: Side, power: RumbleStrength, durationMS: Int = 100) {
@@ -321,41 +361,5 @@ class Robot(hwMap: HardwareMap?) {
     }
 
 
-    init {
-        hardwareMap = hwMap
 
-
-        FL = hardwareMap!!.get(DcMotorEx::class.java, "FL")
-        FR = hardwareMap!!.get(DcMotorEx::class.java, "FR")
-        BL = hardwareMap!!.get(DcMotorEx::class.java, "BL")
-        BR = hardwareMap!!.get(DcMotorEx::class.java, "BR")
-
-        driveMotors = arrayOf(FL, FR, BL, BR)
-
-        headingPIDController.setOutputBounds(-0.1, 0.1)
-        turnPIDController.setOutputBounds(-1.0, 1.0)
-        drivePIDController.setOutputBounds(-1.0, 1.0)
-
-
-        FL.direction = DcMotorSimple.Direction.REVERSE
-        BL.direction = DcMotorSimple.Direction.REVERSE
-
-
-
-        FR.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        FL.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        BR.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-        BL.zeroPowerBehavior = DcMotor.ZeroPowerBehavior.BRAKE
-
-        FR.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-        FL.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-        BR.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-        BL.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
-
-        IMU = hardwareMap!!.get(BNO055IMU::class.java, "imu")
-        val parameters = BNO055IMU.Parameters()
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS
-        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC
-        IMU.initialize(parameters)
-    }
 }
