f0a374e4af778f927ba4df8d2f6f40db8f698892
CJ George <cjgeorge@gmail.com>
2023-11-23 20:32:12 -0600

    We love living, auto tuning is so much fun...

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -34,7 +34,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = true;
+    public static final boolean RUN_USING_ENCODER = false;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(0, 0, 0,
             25.767526266072583);
 
@@ -48,7 +48,7 @@ public class DriveConstants {
      */
     public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 131.5/64.80770150812624; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 7.92; // in
+    public static double TRACK_WIDTH = -7.92; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
